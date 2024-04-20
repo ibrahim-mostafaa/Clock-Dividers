4682e3be5328ca695f598a5230644600e7521c33
ibrahim-mostafaa <167284854+ibrahim-mostafaa@users.noreply.github.com>
2024-04-20 19:11:23 +0200

    Add files via upload

---
+++ Synch_modN_counter/Synch_modN_counter_5/Synch_modN_counter_5.v
@@ -0,0 +1,14 @@
+
+module Synch_modN_counter_5(
+input clk,
+input rst_n,
+output clk_out
+); 
+
+wire n,m; 
+mod5_counter inst_name (.clk(clk), .rst_n(rst_n), .clk_out(n)); 
+dff inst_1(.clk(!clk),.rst_n(rst_n),.d(n),.q(m)); 
+assign clk_out = n|m; 
+
+
+endmodule 
